c27aaea11e7a546ecbf5e11164268ee18d23b41a
Cloud User <ec2-user@ip-172-31-25-53.us-west-2.compute.internal>
2016-10-03 19:04:57 -0400

    debugged index scripts, added product detail pages

--- js/rebatebus.js
+++ js/rebatebus.js
@@ -1,17 +1,21 @@
 /*
  * Rebate Bus Client API demo
  *
- * Note that this code should be run server side in production applications. It only works on demo.rebatebus.com because the 
- * Rebate Bus server sets the Access-Control-Allow-Origin header on requests from that domain.
+ * Note that the AJAX should be run server side in production applications. It only works on demo.rebatebus.com because the 
+ * Rebate Bus server sets the Access-Control-Allow-Origin header on requests from that domain. Including it here so that the 
+ * entire API process will be visible in this demo.
  *
- * The key and UID given below restrict results returned to rebates from Wisconsin's Focus on Energy. You can use it
- * to help develop your app, but it should not be used in production code. 
+ * Also note that stealing this API key and UID won't do you much good - they're tied to the products in the inventory managed by user 1
+ * Feel free to use this API key and UID with these products to develop and debug your own apps. 
  *
  * Mitch Vogel, 9/30/16
  */
 
 var API_KEY = "VmOJXmww6eBGT3XW";
 var UID = 1;
+var products = [
+	1001, 1012, 1013, 1010, 1011, 1009, 1008	
+];
 var bus = {
  downstream: {},
  midstream: {},
@@ -19,18 +23,18 @@ var bus = {
 };
 
 function getUtilities() {
-$.ajax({
-        type: "POST",
-        url: "https://www.rebatebus.com/api/getutilities",
-        data: {"uid": UID, "apikey": API_KEY},
-	crossDomain: true,
-        complete: function(response, stat) {
-                commercialRebateObj = response.commercial;
-                midstreamRebateObj = response.midstream;
-        }, error: function(response, stat) {
-                console.log("error retrieving utilities data from Rebate Bus");
-        }
-});
+	$.ajax({
+		type: "POST",
+		url: "https://www.rebatebus.com/api/getutilities",
+		data: {"uid": UID, "apikey": API_KEY},
+		crossDomain: true,
+		complete: function(response, stat) {
+			bus.utilityDict = JSON.parse(response.responseText);
+			getRebates();
+		}, error: function(response, stat) {
+			console.log("error retrieving utilities data from Rebate Bus");
+		}
+	});
 
 }
 
@@ -42,13 +46,117 @@ function getRebates() {
 		crossDomain: true,
                 complete: function(response, stat) {
                         var rebates = JSON.parse(response.responseText);
-			bus.downstream = rebates.commercial;
+			bus.downstream = rebates.downstream;
 			bus.midstream = rebates.midstream;
+			localizeRebateOffers();
                 }, error: function(response, stat) {
                         console.log("error retrieving rebates data from Rebate Bus");
                 }
         });
 }
 
+/*
+ * Infer a local program by finding the programid at the closest zip code to the geolocated location
+ * After the program has been identified, call setProgramRebates to localize the page to this program
+ */
+function localizeRebateOffers() {
+	var curProgramId;
+	var curUtility;
+	var closestProgram = -1;
+	var closestDifference = 999999999;
+	var curDifference;
+	var curLatDiff, curLngDiff;
+	var i, j;
+	var browseProgramId;
+	var applicablePrograms = [];
+	if (navigator.geolocation) {
+        	navigator.geolocation.getCurrentPosition(function(position) {
+			for (curProgramId in bus.utilityDict) {
+				for (i = 0; i < bus.utilityDict[curProgramId].length; i++)
+				{
+					curUtility = bus.utilityDict[curProgramId][i];
+					if (curUtility.zips) {
+						for (j = 0; j < curUtility.zips.length; j++) {
+							curLatDiff = (curUtility.zips[j].latitude - position.coords.latitude);
+							curLngDiff = (curUtility.zips[j].longitude - position.coords.longitude);
+							curDifference = curLatDiff * curLatDiff + curLngDiff * curLngDiff;
+							if (curDifference < closestDifference) {
+								closestProgram = curProgramId;
+								closestDifference = curDifference;
+							}
+						}
+					}
+				}
+			}
+			if (closestProgram < 0) {
+				alert("Geolocation failed - rebate demos will not work properly");
+			}
+			else {
+				setProgramRebates(closestProgram);
+			}
+
+		});
+    	} else {
+		alert("Geolocation is not supported by this browser - rebate demos will not work properly!");
+    	}
+}
+
+/*
+ * Here we have identified closestProgram as a rebate program with offerings in the inferred zip code
+ *
+ * Now alter the displayed prices and discounts to reflect those available in closestProgram
+ */
+function setProgramRebates(closestProgram) {
+	var i, j;
+	var found;
+	var curProduct;
+	var maxIncentive;
+	for (j = 0; j < bus.downstream.length; j++) {
+		curProduct = bus.downstream[j];
+		found = 0;
+		maxIncentive = {"rebateAmount": -1};
+		// Look for a prescriptive incentive, then a custom. 
+		for (i = 0; i < curProduct.rebates.prescriptive.length; i++) {
+			if (curProduct.rebates.prescriptive[i].programid == closestProgram) {
+				if (curProduct.rebates.prescriptive[i].rebateAmount > maxIncentive.rebateAmount) {
+					maxIncentive = curProduct.rebates.prescriptive[i];
+					found = 1;
+				}
+			}
+		}
+		for (i = 0; i < curProduct.rebates.custom.length; i++) {
+			if (curProduct.rebates.custom[i].programid == closestProgram) {
+				if (curProduct.rebates.custom[i].rebateAmount > maxIncentive.rebateAmount) {
+					maxIncentive = curProduct.rebates.custom[i];
+					found = 1;
+				}
+			}
+		}
+		if (found) {
+			updateRebatePriceQuotes(curProduct.productid, maxIncentive);
+		}
+	}
+}
+
+/*
+ * We've found a rebate that applies to productid in the program we're localizing to - update to DOM to reflect the discount
+ */
+function updateRebatePriceQuotes(productid, incentive) {
+	$("#" + productid + " .pric1").append("<del>$" + incentive.msrp + "</del>");
+	$("#" + productid + " .pric2").text("$" + (incentive.msrp - incentive.rebateAmount).toFixed(2));
+	$("#" + productid + " .disc").text("$" + incentive.rebateAmount + " rebate from " + incentive.program);
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
+
 getUtilities();
-getRebates();
